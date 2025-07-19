b0136a28082f8cc83da9feb745b763c39655be80
Nico Borgsmüller <me@kaenganxt.dev>
2025-07-19 15:18:14 +0200

    Add support for arctis nova 7

---
+++ arctis_manager/devices/device_arctis_nova_7.py
@@ -0,0 +1,26 @@
+from arctis_manager.device_manager import (DeviceState, DeviceManager,
+                                           DeviceStatus, InterfaceEndpoint)
+
+BATTERY_MIN = 0x00
+BATTERY_MAX = 0x04
+
+
+class Arctis7PlusDevice(DeviceManager):
+    def get_device_product_id(self) -> int:
+        return 0x2202
+
+    def get_device_name(self) -> str:
+        return 'Arctis Nova 7'
+
+    def manage_input_data(self, data: list[int], endpoint: InterfaceEndpoint) -> DeviceState:
+        if endpoint == self.utility_guess_endpoint(7, 'in'):
+            # This probably needs some more work. Taken from original project.
+            # see https://github.com/Sapd/HeadsetControl/blob/master/src/devices/steelseries_arctis_7_plus.c#L103
+
+            return DeviceState(data[1] / 100, data[2] / 100, 1, 1, DeviceStatus())
+
+    def get_endpoint_addresses_to_listen(self) -> list[InterfaceEndpoint]:
+        return [self.utility_guess_endpoint(7, 'in')]
+
+    def get_request_device_status(self):
+        return self.utility_guess_endpoint(7, 'out'), [0x06, 0xb0]
